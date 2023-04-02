bb02586053eca69c167042ee61ba58a64770c520
ngocmanh <ngocmanh445@gmail.com>
2023-04-02 19:17:21 +0700

    add

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    let toDoContainer = $(".todo-container");
+    let addBox = toDoContainer.find(".add-box");
+    let listBox = toDoContainer.find(".list-box");
+    let inputAdd = addBox.find(".input-add")
+    let buttonAdd = addBox.find(".button-add")
+    
+
+    listBox.find(".item .custome-check").click(function () {
+        let $this = $(this);
+        $this.closest(".item").toggleClass("active")
+    });
+    buttonAdd.click(function () {
+        let value = inputAdd.val();
+        listBox.prepend("<div>helo</div");
+    });
+    inputAdd.keyup(function (event) {
+        if (event.which == 13) {
+            listBox.prepend("<div>helo</div");
+        }
+    })
+
+});
